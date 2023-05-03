2bdde8c376097bade0944c1d10380e459b162849
CristobalJJG <cristobal.jimenez101@alu.ulpgc.es>
2023-05-03 19:35:39 +0100

    Modal de inicio de sesión hecho

--- academy_war/src/app/login/login.component.ts
+++ academy_war/src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['../header/header.component.scss', '../register/register.component.scss']
 })
 export class LoginComponent {
 
