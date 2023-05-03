fbd16c3e660fe2936780671ebbeb26279419a4d9
CristobalJJG <cristobal.jimenez101@alu.ulpgc.es>
2023-05-03 15:57:26 +0100

    Comienzo del header

--- academy_war/src/app/app.module.ts
+++ academy_war/src/app/app.module.ts
@@ -7,15 +7,23 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { environment } from 'src/environments/environment';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
