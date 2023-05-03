bce625aec284ce45bcc64feeeed77217913795ab
CristobalJJG <cristobal.jimenez101@alu.ulpgc.es>
2023-05-03 19:25:11 +0100

    Modal de registro hecho

--- academy_war/src/app/header/header.component.ts
+++ academy_war/src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ModalService } from '../services/modal.service';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  constructor(private modal: ModalService) { }
+
+  openLogin() {
+    this.modal.openDialog(LoginComponent, "500px", "600px");
+  }
+
+  openRegister() {
+    this.modal.openDialog(RegisterComponent, "500px", "600px");
+  }
 
 }
