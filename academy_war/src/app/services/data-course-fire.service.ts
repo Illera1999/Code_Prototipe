9a23ceb95803f3d365684563f1afaaac4ee328f5
Jose Illera <illerarodriguez1999@gmail.com>
2023-05-08 12:00:46 +0100

    Servicios de la tabla cursos

--- academy_war/src/app/services/data-course-fire.service.ts
+++ academy_war/src/app/services/data-course-fire.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { collection, query, getFirestore, getDocs } from "firebase/firestore";
+import { AuthService } from './auth.service';
+import { Course } from '../class/course'; 
+import { Lesson } from '../class/lesson'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataCourseFireService {
 
-  constructor() { }
+  private db = getFirestore(AuthService.getApp());
+  private courses: Course[] = [];
+
+  async setAllCourse(){
+    try{
+      const q = query(collection(this.db,"course"));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        const aux = new Course (
+          doc.id,
+          doc.data()["name"],
+          doc.data()["description"],
+          doc.data()["programmingLanguage"],
+          new Lesson(doc.data()["lessons"].id,doc.data()[""]),
+          doc.data()["challenges"],
+          doc.data()["users"]);
+        
+        this.courses.push(aux);
+
+        ////console.log(doc.id, " => ", doc.data()["name"]);
+      });
+      console.log("Get collection Course");
+    } catch (e) {
+      console.error("Error geting document", e);
+    }
+    return this.courses;
+  }
+
 }
