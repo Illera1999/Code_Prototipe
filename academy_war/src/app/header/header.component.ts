0b4178900553965ada8c7e395ce640e7414548a2
m2artigas <m2artigas@gmail.com>
2023-05-10 16:29:39 +0100

    quick fix

--- academy_war/src/app/header/header.component.ts
+++ academy_war/src/app/header/header.component.ts
@@ -4,8 +4,6 @@ import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
 import { User } from '../class/user';
 import { AuthService } from '../services/auth.service';
-import { DataCourseFireService } from '../services/data-course-fire.service';
-import { Course } from '../class/course';
 import { Router } from '@angular/router';
 import { DataCourseFireService } from '../services/data-course-fire.service';
 import { Course } from '../class/course';
@@ -19,7 +17,7 @@ export class HeaderComponent {
 
   protected user: User | undefined;
 
-  constructor(private modal: ModalService, private auth: AuthService, private db: DataCourseFireService, private db: DataCourseFireService ) {
+  constructor(private modal: ModalService, private auth: AuthService, private db: DataCourseFireService, private router:Router) {
     let temp = localStorage.getItem("user");
     if (temp) {
       let v = JSON.parse(temp)
