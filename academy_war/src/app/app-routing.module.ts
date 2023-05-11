0d4191a1ac9ac5cb287fc858aeb2afc0c08084d5
m2artigas <m2artigas@gmail.com>
2023-05-11 20:04:51 +0100

    lesson display

--- academy_war/src/app/app-routing.module.ts
+++ academy_war/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CourseComponent } from './course/course.component';
 import { LessonDropdownComponent } from './lesson/lesson-dropdown/lesson-dropdown.component';
+import { LessonComponent } from './lesson/lesson.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'course/:id', component: CourseComponent }
+  { path: 'course/:id', component: CourseComponent },
+  { path: 'lesson', component: LessonComponent }
 ];
 
 @NgModule({
--- academy_war/src/app/lesson/lesson.component.ts
+++ academy_war/src/app/lesson/lesson.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { ModalService } from '../services/modal.service';
+import { DataCourseFireService } from '../services/data-course-fire.service';
+import { Course } from '../class/course';
 
 @Component({
   selector: 'app-lesson',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./lesson.component.scss']
 })
 export class LessonComponent {
+  course!: Course | null;
+
   protected description : any;
+  constructor(private db: DataCourseFireService) {
+    db.getParticularCourse("Course C").then((data: Course | null) => {
+      if (data != null) this.course = data;
+    })
+  }
+
 }
