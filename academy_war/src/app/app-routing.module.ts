5cf1c25dbdf239d35baff9751b68f78ec7cf0d43
JorgeAM1951 <100132639+JorgeAM1951@users.noreply.github.com>
2023-05-14 01:17:49 +0100

    Challenge component added

--- academy_war/src/app/app-routing.module.ts
+++ academy_war/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { CourseComponent } from './course/course.component';
 import { LessonDropdownComponent } from './lesson/lesson-dropdown/lesson-dropdown.component';
 import { LessonComponent } from './lesson/lesson.component';
+import { ChallengeComponent } from './challenge/challenge.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'course/:id', component: CourseComponent },
-  { path: 'lesson', component: LessonComponent }
+  { path: 'lesson', component: LessonComponent },
+  { path: 'challenge', component: ChallengeComponent },
+
 ];
 
 @NgModule({
