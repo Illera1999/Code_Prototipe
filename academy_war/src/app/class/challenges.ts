4ecf275fba4ee3fb1c90a8a91f81f54830a8088c
JorgeAM1951 <100132639+JorgeAM1951@users.noreply.github.com>
2023-05-15 12:24:18 +0100

    provisional challenge

--- academy_war/src/app/class/challenges.ts
+++ academy_war/src/app/class/challenges.ts
@@ -1,15 +1,16 @@
+import { Title } from "@angular/platform-browser";
 import { Stage } from "./stage";
 
 export class Challenge {
-    private id: string = "";
-    private title: string = "";
-    private description: string = "";
-    private executionTime: Date = new Date();
-    private durationTime: Date = new Date();
-    private score: number = 0;
-    private stage: Stage = Stage.HIGH;
+    private id: string;
+    private title: string;
+    private description: string;
+    //private executionTime: Date = new Date();
+    //private durationTime: Date = new Date();
+    private score: number;
+    private stage: string;
 
-    constructor(id: string, title: string, description: string, score: 0, stage: Stage) {
+    constructor(id: string, title: string, description: string, score: number, stage: string) {
         this.id = id;
         this.title = title;
         this.description = description;
@@ -20,16 +21,16 @@ export class Challenge {
     public getId() { return this.id }
     public getTitle() { return this.title }
     public getDescription() { return this.description }
-    public getExecutionTime() { return this.executionTime }
-    public getDurationTime() { return this.durationTime }
+    //public getExecutionTime() { return this.executionTime }
+    //public getDurationTime() { return this.durationTime }
     public getScore() { return this.score }
     public getStage() { return this.stage }
 
     public setId(id: string) { this.id = id }
     public setTitle(title: string) { this.title = title }
     public setDescription(description: string) { this.description = description }
-    public setExecutionTime(executionTime: Date) { this.executionTime = executionTime; }
-    public setDurationTime(durationTime: Date) { this.durationTime = durationTime }
+    //public setExecutionTime(executionTime: Date) { this.executionTime = executionTime; }
+    //public setDurationTime(durationTime: Date) { this.durationTime = durationTime }
     public setScore(score: number) { this.score = score }
     public setStage(stage: Stage) { this.stage = stage }
 }
