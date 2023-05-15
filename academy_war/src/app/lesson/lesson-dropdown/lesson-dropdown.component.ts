30178d89a2eda54f7df3d4f0e40d831bead74160
carsd3 <littleguitar2001@gmail.com>
2023-05-15 19:35:36 +0100

    sidebar lesson terminado

--- academy_war/src/app/lesson/lesson-dropdown/lesson-dropdown.component.ts
+++ academy_war/src/app/lesson/lesson-dropdown/lesson-dropdown.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from 'src/app/class/course';
 import { DataCourseFireService } from 'src/app/services/data-course-fire.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lesson-dropdown',
@@ -17,17 +18,25 @@ export class LessonDropdownComponent implements OnInit {
 
   course!: Course | null;
 
-  constructor(private db: DataCourseFireService) {
+  constructor(private db: DataCourseFireService, protected router: Router) {
     db.getParticularCourse("Course C").then((data: Course | null) => {
       if (data != null) this.course = data;
     })
   }
 
   ngOnInit(): void {
+      console.log(document.location.href);
       
   }
 
   toggleSideNav() {
     return this.hideSideNav = !this.hideSideNav;
+  }
+
+  openLesson(id:String) {
+    this.router.navigate(['/lesson/', id]);
+    setTimeout(() => {
+      window.location.reload();
+    }, 1);
   }
 }
--- academy_war/src/app/lesson/lesson.component.ts
+++ academy_war/src/app/lesson/lesson.component.ts
@@ -13,43 +13,26 @@ import { Lesson } from '../class/lesson';
 export class LessonComponent {
   course: Course[] = [];
 
-  protected lesson: Lesson[] = new Array(1);;
+  protected lesson: Lesson[] = new Array(1);
 
   protected description : any;
   constructor(private db: DataCourseFireService) {
+    let lessonName = document.location.href.split("/").pop()?.replaceAll("%20", " ");
     db.getParticularCourse("Course C").then((data: Course | null) => {
       if (data != null) {
         this.course[0] = data;
         for (let i = 0; i < data.getLessons().length; i++) {
+          console.log(data.getLessons()[i].getTitle());
           if (data.getLessons()[i].getTitle() == lessonName) {
             this.lesson[0] = data.getLessons()[i];
           }
         }
       }
-      console.log(this.lesson); 
-    })
-    let lessonName = document.location.href.split("/").pop()?.replaceAll("%20", " ");
+      // console.log(this.lesson); 
+    });
     console.log(this.course)
     console.log(this.course.at(0))
     
   }
-
-  // findLesson(lesson: any){
-  //   let les : Lesson = new Lesson({datos: {
-  //     usersCompleted: new Array<string>(),
-  //     id: "",
-  //     title: "",
-  //     description: "",
-  //     stage: ""
-  //   }});
-  //   console.log(this.course);
-  //   for (let i = 0; i < this.course!.getLessons().length; i++) {
-  //     if (this.course!.getLessons()[i] == lesson) {
-  //       return this.course!.getLessons()[i];
-  //     }
-  //   }
-  //   console.log(les)
-  //   return les;
-  // }
 
 }
