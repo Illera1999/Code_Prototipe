556619f2606e2d47ab4351515671e25ef7f73d61
JorgeAM1951 <100132639+JorgeAM1951@users.noreply.github.com>
2023-05-16 12:05:38 +0100

    update to db status

--- academy_war/src/app/class/challenges.ts
+++ academy_war/src/app/class/challenges.ts
@@ -1,3 +1,4 @@
+import { ComponentDecorator } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { Stage } from "./stage";
 
@@ -5,10 +6,9 @@ export class Challenge {
     private id: string;
     private title: string;
     private description: string;
-    //private executionTime: Date = new Date();
-    //private durationTime: Date = new Date();
     private score: number;
     private stage: string;
+    private code: string;
 
 
     constructor(datos: any) {
@@ -19,21 +19,18 @@ export class Challenge {
         this.description = data.description;
         this.score = data.score;
         this.stage = data.stage;
+        this.code = data.code;
     }
 
     public getId() { return this.id }
     public getTitle() { return this.title }
     public getDescription() { return this.description }
-    //public getExecutionTime() { return this.executionTime }
-    //public getDurationTime() { return this.durationTime }
     public getScore() { return this.score }
     public getStage() { return this.stage }
 
     public setId(id: string) { this.id = id }
     public setTitle(title: string) { this.title = title }
     public setDescription(description: string) { this.description = description }
-    //public setExecutionTime(executionTime: Date) { this.executionTime = executionTime; }
-    //public setDurationTime(durationTime: Date) { this.durationTime = durationTime }
     public setScore(score: number) { this.score = score }
     public setStage(stage: Stage) { this.stage = stage }
 }
